e5fce2f4361fc7c6fda26599f54092d357cc3bea
Harshitha <harshitha_kapalavayi@srmap.edu.in>
2025-05-04 18:41:20 -0500

    initial changes

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="absolute inset-0 flex justify-center items-center bg-gray-100">
+      
+      <iframe title="Art Career Insights Dashboard" width="1500" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiYzI4NWQzOTQtNjE1Yy00N2Q2LWI1MzgtMGNmMjc3N2I5Yjc1IiwidCI6IjgzYzE3ZDkwLTU4MzItNDhiOS04ZWYyLWIyMGY3NjJlYWZiNSIsImMiOjN9&navContentPaneEnabled=false" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+    </div>
+  );
+};
+
+export default Dashboard;
